31a42bbda22c05b8c1b9b1824d3db94ae73c6d3a
Ethan Lewis <3thanlewis@gmail.com>
2014-10-10 15:17:52 -0700

    Added necessary db fields for user association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008024926) do
+ActiveRecord::Schema.define(version: 20141010221513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 20141008024926) do
     t.string   "address",                default: ""
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "account_id"
+    t.string   "account_type"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
