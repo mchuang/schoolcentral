fa530629f82f102cb715e96a79f539f5606fb9d3
Ethan Lewis <3thanlewis@gmail.com>
2014-10-12 13:03:19 -0700

    Increased password hash iterations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :lockable, :timeoutable
+         :lockable, :timeoutable, :stretches => 12
 
   # Belongs to Teachers/Students/Admins (as account)
   belongs_to :account, :polymorphic => true
