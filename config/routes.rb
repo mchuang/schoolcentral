f3d34aec775e872cc1e224d9c1ea9ac4e0c5bf80
Vincent Oe <vincent.satoru.oe@gmail.com>
2014-10-19 16:40:24 -0700

    added classrooms controller and resource

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   root 'static#home'
 
   #Adds standard
-  resources :user
+  resources :user, :classrooms
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
