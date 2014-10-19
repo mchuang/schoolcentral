9cc25cd1e55e709b51d18d853cb14e8302f4dc2b
dlaroue <dlaroue4@berkeley.edu>
2014-10-19 16:22:07 -0700

    refactoring users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  after_action :verify_policy_scoped, :only => :index
-  after_action :verify_authorized, :except => :index
+  #after_action :verify_policy_scoped, :only => :index
+  #after_action :verify_authorized, :except => :index
 
   def after_sign_in_path_for(resource)
     dashboard_index_path
