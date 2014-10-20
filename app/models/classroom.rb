5ac24c2e850ac286f92e1973f0b50768edaf058b
2011dkang1 <britishwristwatch@gmail.com>
2014-10-19 17:36:25 -0700

    added a few comments

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,5 +1,5 @@
 class Classroom < ActiveRecord::Base
-	#more then one teacher and more then one student per class, but teahcers and
+	#more then one teacher and more then one student per class, but teachers and
 	#students also have more then one class
 	has_and_belongs_to_many :teachers
 	has_and_belongs_to_many :students
