14888af04498b63b32fb0aa110729ef72e565b44
Ethan Lewis <3thanlewis@gmail.com>
2014-10-19 17:20:02 -0700

    Added school model with accessors for account types

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,4 +1,6 @@
 class Classroom < ActiveRecord::Base
+    belongs_to :school
+    
 	#more then one teacher and more then one student per class, but teahcers and
 	#students also have more then one class
 	has_and_belongs_to_many :teachers
---
+++ app/models/school.rb
@@ -0,0 +1,16 @@
+class School < ActiveRecord::Base
+    has_many :classrooms
+    has_many :users
+
+    def admins
+        Admin.where(id: users.where(account_type: "Admin").select(:account_id))
+    end
+
+    def teachers
+        Teacher.where(id: users.where(account_type: "Teacher").select(:account_id))
+    end
+
+    def students
+        Student.where(id: users.where(account_type: "Student").select(:account_id))
+    end
+end
