b6cb8e18ee3b8747322ab034d490cd968d52bf45
Ethan Lewis <3thanlewis@gmail.com>
2014-10-21 19:39:31 -0700

    Fixed spacing in policy classes

--- app/policies/admin_policy.rb
+++ app/policies/admin_policy.rb
@@ -1,22 +1,15 @@
 class AdminPolicy < ApplicationPolicy
   class Scope < Scope
-  	attr_reader :user, :scope
-	
-	  def initialize(user,scope)
-  		@user = user
-  		@scope = scope
-  	end
-    
     def resolve
-    	type = user.account_type
-    	case type
-    		when 'Admin'
-        	scope.all
-    		when 'Teacher'
-    			scope.all
-    		when 'Student'
-    			scope.all
-    	end
+      type = user.account_type
+      case type
+        when 'Admin'
+          scope.all
+        when 'Teacher'
+          scope.all
+        when 'Student'
+          scope.all
+      end
     end
   end
 end
--- app/policies/student_policy.rb
+++ app/policies/student_policy.rb
@@ -1,25 +1,18 @@
 class StudentPolicy < ApplicationPolicy
   class Scope < Scope
-  	attr_reader :user, :scope
-
-    def initialize(user,scope)
-  		@user = user
-  		@scope = scope
-  	end
-    
     def resolve
-    	type = user.account_type
-    	case type
+      type = user.account_type
+      case type
     		# Admins can see everything
-    		when 'Admin'
-    			scope.all
+        when 'Admin'
+          scope.all
     		# Teachers can see only their own students
-    		when 'Teacher'
+        when 'Teacher'
           scope.where(id: user.account.students.map(&:id))
         # Students can see only themselves
-    		when 'Student'
-    			scope.where(id: user.account_id)
-    	end
+        when 'Student'
+          scope.where(id: user.account_id)
+      end
     end
   end
 end
