13cb50492701aae38a6b9057da63ea222318426e
Michael Chuang <michaelchuang@pioneers.berkeley.edu>
2014-10-22 18:24:10 -0700

    Base admin dashboard added

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,18 @@
 class DashboardController < ApplicationController
+	def admin_dashboard
+		@infoType = params[:infoType]
+		case params[:infoType]
+			when "Students"
+				@info = Students.all
+			when "Teachers"
+				@info = Teachers.all
+			when "Classrooms"
+				@info = Clasrooms.all
+		end
+		render 'admin_dashboard'
+
+	def admin_dashboard_classroom
+		@students = Students.all
+		@teachers = Teachers.all
+
 end
