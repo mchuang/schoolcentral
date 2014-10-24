60762cedb69da3e89ecc38d77e8c44eca9ae37cf
Ethan Lewis <3thanlewis@gmail.com>
2014-10-23 19:28:18 -0700

    Set default student_capacity

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021053503) do
+ActiveRecord::Schema.define(version: 20141024022204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20141021053503) do
     t.datetime "time"
     t.string   "location"
     t.string   "description"
-    t.integer  "student_capacity"
+    t.integer  "student_capacity", default: 30
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "school_id"
