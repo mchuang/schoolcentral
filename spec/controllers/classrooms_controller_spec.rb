278ae524fa915d8d148e2803dd5e56940c9f4c27
2011dkang1 <britishwristwatch@gmail.com>
2014-10-23 22:43:37 -0700

    added controller for classrooms

--- spec/controllers/classrooms_controller_spec.rb
+++ spec/controllers/classrooms_controller_spec.rb
@@ -2,4 +2,30 @@ require 'rails_helper'
 
 RSpec.describe ClassroomsController, :type => :controller do
 
+
+
+  before(:each) do
+    @class0 = FactoryGirl.create(:classroom)
+    # @admin0   = FactoryGirl.create(:admin_user,   email: "", identifier: "admin0")
+    @teacher0 = FactoryGirl.create(:teacher_user, email: "", identifier: "teacher0")
+    @student0 = FactoryGirl.create(:student_user, email: "", identifier: "student0")
+
+    # @teacher0.account.classrooms << @class0
+    @class0.teachers << @teacher0.account
+    @class0.students << @student0.account
+  end
+
+  describe "ClassroomsController" do
+    it "get_teacher classrooms" do
+      sign_in @teacher0
+      get :show, :id => @class0.id
+      expect(assigns(:classrooms)).to eq(@teacher0.account.classrooms)
+    end
+    it "get_student classrooms" do
+      sign_in @student0
+      get :show, :id => @class0.id
+      expect(assigns(:classrooms)).to eq(@student0.account.classrooms)
+    end
+  end
+
 end
