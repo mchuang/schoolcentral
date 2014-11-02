9f054daa29948747c6f230d144a5620f39dfd8a7
Jeff DeFond <jeffdefond@gmail.com>
2014-11-02 14:27:00 -0800

    event added

---
+++ spec/factories/events.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :event do
+    name "MyString"
+    startime "2014-11-02 14:23:53"
+    endtime "2014-11-02 14:23:53"
+    description "MyString"
+  end
+end
