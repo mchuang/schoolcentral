d0478e3c318a18ad04994479680779a5d703e769
2011dkang1 <britishwristwatch@gmail.com>
2014-11-02 14:44:56 -0800

    actually added files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024022204) do
+ActiveRecord::Schema.define(version: 20141102223242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,15 @@ ActiveRecord::Schema.define(version: 20141024022204) do
     t.integer "classroom_id", null: false
   end
 
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.datetime "startime"
+    t.datetime "endtime"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "schools", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
@@ -58,6 +67,15 @@ ActiveRecord::Schema.define(version: 20141024022204) do
   end
 
   create_table "students", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "submissions", force: true do |t|
+    t.string   "filename"
+    t.integer  "grade"
+    t.integer  "assignment_id"
+    t.integer  "student_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
