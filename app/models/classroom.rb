9e7516ac8ffd706f4d1ec316f256049c4da5cc84
Jeff DeFond <jeffdefond@gmail.com>
2014-11-02 14:34:02 -0800

    event added correctly this time

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -9,6 +9,7 @@ class Classroom < ActiveRecord::Base
 	has_and_belongs_to_many :students
 
     has_many :attendance
+    has_many :events
 
     validates :name, presence: true, uniqueness: true
     validates :student_capacity, numericality: { greater_than_or_equal_to: 0 }
