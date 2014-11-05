5df736660782fa5ed05b2300e5185291f9da5b70
dlaroue <dlaroue4@berkeley.edu>
2014-11-04 21:09:00 -0800

    Added partials for teacher and student grades pages

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,4 +2,12 @@ class Assignment < ActiveRecord::Base
   belongs_to :teacher
   belongs_to :classroom
   has_many :submissions
+
+  def get_submission(student_id)
+  	submissions.find_by_student_id(student_id)
+  end
+
+
+
+
 end
