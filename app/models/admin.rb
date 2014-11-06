15323ec64351fce15a45691c1d79b52d582fc307
Ethan Lewis <3thanlewis@gmail.com>
2014-11-05 22:08:25 -0800

    Added event support and date fetching

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,8 @@
 class Admin < ActiveRecord::Base
 	#Admin has one user object, (as account)
 	has_one :user, :as => :account, :dependent => :destroy
+
+    def events
+        user.school.classrooms.map {|cls| cls.events}.flatten
+    end
 end
