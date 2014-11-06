14f878f56722cb94917fd9f31d8f808964f0282c
Vincent Oe <vincent.satoru.oe@gmail.com>
2014-11-06 03:41:19 -0800

    added empty calendar

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,27 @@
 class Event < ActiveRecord::Base
-  belongs_to :owner, :polymorphic => true
+  	belongs_to :owner, :polymorphic => true
 
-  belongs_to :classroom
+  	belongs_to :classroom
+
+  	def self.get_dates_for_month(year, month)
+		firstDate = Date.new(year, month, 1)
+		if !firstDate.sunday?
+			firstDate = firstDate.beginning_of_week - 1
+		end
+		lastDate = Date.new(year, month, -1)
+		if lastDate.sunday?
+			lastDate = lastDate + 6
+		elsif !lastDate.saturday?
+			lastDate = lastDate.end_of_week - 1
+		end
+
+		dateArr = (firstDate..lastDate).to_a
+		i = 0
+		weekArr = []
+		while i < dateArr.length
+			weekArr.push(dateArr.slice(i, 7))
+			i += 7
+		end
+		return weekArr
+	end
 end
