bafde077a0134c50b3256988b5128f072d1e951e
Ethan Lewis <3thanlewis@gmail.com>
2014-11-19 16:42:11 -0800

    Added model functions to send emails

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,28 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, :type => :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "custom_email" do
+    before(:each) do
+      UserMailer.custom_email(
+        from: "from_addr@fake.com",
+        to: "to_addr@fake.com",
+        cc: "cc_addr@fake.com",
+        bcc: "bcc_addr@fake.com",
+        subject: "Test Subject",
+        message: "Test Content"
+      ).deliver
+    end
+
+    after(:each) do
+      ActionMailer::Base.deliveries.clear
+    end
+
+    it { expect(ActionMailer::Base.deliveries.count).to eq(1) }
+    it { expect(ActionMailer::Base.deliveries.last.subject).to eq("Test Subject") }
+    it { expect(ActionMailer::Base.deliveries.last.from).to    match_array(["from_addr@fake.com"]) }
+    it { expect(ActionMailer::Base.deliveries.last.to).to      match_array(["to_addr@fake.com"]) }
+    it { expect(ActionMailer::Base.deliveries.last.cc).to      match_array(["cc_addr@fake.com"]) }
+    it { expect(ActionMailer::Base.deliveries.last.bcc).to     match_array(["bcc_addr@fake.com"]) }
+  end
 end
