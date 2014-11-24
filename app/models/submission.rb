6d99e6098735472c94d2c0210b774eaf0def10d5
Jeff DeFond <jeffdefond@gmail.com>
2014-11-23 17:59:44 -0800

    classroom controller tests now pass

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,7 +2,7 @@ class Submission < ActiveRecord::Base
   belongs_to :assignment
   belongs_to :student
   has_attached_file :file
-  validates_attachment_content_type :file, :content_type => ['application/pdf', 'application/msword', 'text/plain']
+  validates_attachment_content_type :file, :content_type => ['application/pdf', 'application/msword', 'text/plain', 'application/zip']
   validates :assignment_id, presence: true
   validates :student_id,    presence: true
   validate  :positive_grade
